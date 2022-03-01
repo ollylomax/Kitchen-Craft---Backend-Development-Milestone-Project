ed703842f217ef7f042409976baa2fef8449a0db
ollylomax <ollylomax@yahoo.com>
2022-03-01 14:21:42 +0000

    test: Create temp function in app.py to check heroku is linked with git repo

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ if os.path.exists("env.py"):
 # Create instance of flask
 app = Flask(__name__)
 
+
+@app.route("/")
+def functional():
+    return "functional"
+
+
 # Where and how to run out app
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
