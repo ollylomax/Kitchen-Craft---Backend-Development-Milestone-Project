c429beb37821279efe816e3cf030c1867aac865d
ollylomax <ollylomax@yahoo.com>
2022-03-02 15:15:20 +0000

    Wire up flask with mongodb and check with temp home page function

--- app.py
+++ app.py
@@ -3,20 +3,39 @@ import os
 # Import flask
 from flask import Flask
 
+# Wire up flask with mongodb
+from flask_pymongo import PyMongo
+
+# Render objectid from mongodb docs
+from bson.objectid import ObjectId
+
 # Import env file only if path exists
-if os.path.exists("env.py"):
+if os.path.exists('env.py'):
     import env
 
 # Create instance of flask
 app = Flask(__name__)
 
+# Configure database name
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+# Configure connection string
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+# Get secret key
+app.secret_key = os.environ.get('SECRET_KEY')
 
-@app.route("/")
-def functional():
-    return "functional"
+# Create instance of pymongo and pass in flask app object
+mongo = PyMongo(app)
+
+# Default route decorator
+@app.route('/')
+# Home page route decorator
+@app.route('/home')
+# Temp function
+def home():
+    return 'home page'
 
 
-# Where and how to run out app
+# Where and how to run app
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
