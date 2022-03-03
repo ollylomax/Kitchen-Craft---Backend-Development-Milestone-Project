ca6e76e3172ee190b05843cb43e4d9a0dd4b9a75
ollylomax <ollylomax@yahoo.com>
2022-03-03 11:49:34 +0000

    style: Add and style footer with social links block, add automatic year update to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
     $(".dropdown-trigger").dropdown();
+    // Get current year in footer copyright by assigning new date object to year id
+    document.getElementById("year").innerHTML = new Date().getFullYear();
   });
+
+  
