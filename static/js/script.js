d0d7b57aed269a60df6255edc2066c976e3e3a71
ollylomax <ollylomax@yahoo.com>
2022-03-06 10:05:23 +0000

    fix: Adjust base.html nav links and add character counter script to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
     $('select').formSelect();
+    $('textarea').characterCounter();
   });
 
   
