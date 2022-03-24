aac6c820d9787ea1deaba8ee42e6dbfa38f83f6b
ollylomax <ollylomax@yahoo.com>
2022-03-24 13:56:20 +0000

    Remove password validator function from script.js and insert into newly created pwvalidator.js file

--- static/js/script.js
+++ static/js/script.js
@@ -11,24 +11,6 @@ $(document).ready(function () {
   $('textarea').characterCounter();
   $('.modal').modal();
 
-  // Assign password id input to variable
-  let password = document.getElementById("password")
-  // Assign pconfirm id input to variable
-  let pconfirm = document.getElementById("pconfirm")
-
-  // Function to check password against pconfirm
-  function confirmPassword() {
-    if (password.value != pconfirm.value) { // Conditional statement to check if passwords do not match
-      pconfirm.setCustomValidity("Your Password does not match"); // Method to display custom validity error
-    } else { // If they do match
-      pconfirm.setCustomValidity(''); // Empty string meaning no validity error
-    }
-  }
-  // Function is called if password input is changed
-  password.onchange = confirmPassword;
-  // Function called after every keystroke
-  pconfirm.onkeyup = confirmPassword;
-
   // Code Institute Materialize Select Validator from task manager walkthrough lessons
   validateMaterializeSelect();
 
